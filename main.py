381209ec537c381b5668b83ae3c11e83a2fbb555
BrianSalk <brian.salkas@gmail.com>
2024-08-25 22:57:03 -0500

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
+from time import sleep
+import streamlit as st
+
+URL = "https://www.google.com/"
+TIMEOUT = 20
+
+st.title("Test Selenium")
+
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
+driver.get(URL)
+
+id = "gbqfbb"
+sleep(2)
+button = driver.find_element(by=By.ID, value = id)
+st.write(button.get_property("value"))
